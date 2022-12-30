afdf229b661a3609568b25c40ed9de97725c2285
Yasaswini Vulisetti <119730496+22P31A0543@users.noreply.github.com>
2022-12-30 19:19:35 +0530

    #include<stdio.h> int main() {     char a;     scanf("%c",&a);     if(a=='v')     {         printf("violet");     }     else if(a=='i')     {         printf("Indigo");     }     else if(a=='B')     {         printf("Blue");     }     else if(a=='G')     {         printf("Green");     }     else if(a=='Y')     {         printf("Yellow");     }     else if(a=='O')     {         printf("Orange");     }     else if(a=='R')     {         printf("Red");     }     else     {     printf("-1");     } }

---
+++ Print_the_color_name.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if(a=='v')
+    {
+        printf("violet");
+    }
+    else if(a=='i')
+    {
+        printf("Indigo");
+    }
+    else if(a=='B')
+    {
+        printf("Blue");
+    }
+    else if(a=='G')
+    {
+        printf("Green");
+    }
+    else if(a=='Y')
+    {
+        printf("Yellow");
+    }
+    else if(a=='O')
+    {
+        printf("Orange");
+    }
+    else if(a=='R')
+    {
+        printf("Red");
+    }
+    else
+    {
+    printf("-1");
+    }
+}
