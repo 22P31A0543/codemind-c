cd8b127b429df70dbc9ee2f6c877dfcec34ca930
Yasaswini Vulisetti <119730496+22P31A0543@users.noreply.github.com>
2023-05-22 22:08:26 +0530

    #include<stdio.h> int main() {     int a[20],i,j,k,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 for(k=j;k<n-1;k++)                 {                     a[k]=a[k+1];                 }                 n--;                 j--;             }         }     }     for(i=0;i<n;i++)     {         printf("%d ",a[i]);     } }

---
+++ Remove_duplicates_from_an_array__.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a[20],i,j,k,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                for(k=j;k<n-1;k++)
+                {
+                    a[k]=a[k+1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
