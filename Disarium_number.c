9303a03c9e7523d9f079ae25d4c24f9772234acf
Yasaswini Vulisetti <119730496+22P31A0543@users.noreply.github.com>
2023-05-23 21:37:22 +0530

    #include<stdio.h> int main() {     int n,r,sum=0,a,c=1,p=1,i;     scanf("%d",&n);     a=n;     while(a!=0)     {         r=a%10;         sum=(sum*10)+r;         a=a/10;     }     a=sum;     sum=0;     while(a!=0)     {         r=a%10;         for(i=1;i<=c;i++)         {             p=p*r;         }         sum=sum+p;         a=a/10;         c++;         p=1;     }     if(sum==n)     {         printf("True");     }     else     {         printf("False");     } }

--- Disarium_number.c
+++ Disarium_number.c
@@ -1,26 +1,35 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-    int n,t,sum=0,c=0;
+    int n,r,sum=0,a,c=1,p=1,i;
     scanf("%d",&n);
-    t=n;
-    while(t!=0)
+    a=n;
+    while(a!=0)
     {
-    t=t/10;
-    c=c+1;
+        r=a%10;
+        sum=(sum*10)+r;
+        a=a/10;
     }
-t=n;
-while(c!=0){
-    int rem=t%10;
-    t=t/10;
-    sum=sum+pow(rem,c);
-    c=c-1;
-}
-if(sum==n){
-    printf("True");
-}else{
-    printf("False");
-}
- 
+    a=sum;
+    sum=0;
+    while(a!=0)
+    {
+        r=a%10;
+        for(i=1;i<=c;i++)
+        {
+            p=p*r;
+        }
+        sum=sum+p;
+        a=a/10;
+        c++;
+        p=1;
+    }
+    if(sum==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
 }
