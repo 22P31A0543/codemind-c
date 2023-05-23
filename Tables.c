cf0b68f31d4e8582d6b915012af194c34b2547ab
Yasaswini Vulisetti <119730496+22P31A0543@users.noreply.github.com>
2023-05-23 21:41:20 +0530

    #include<stdio.h> int main()
    {
    
    int n,m,i;
    
    scanf("%d %d", &n,&m);
    
    for(i=1;i<=m;i++)
    
    {
    
    if(i%2!=0)
    
    printf("%d x %d = %d
    ",n,i,n*i);
    }
    }

--- Tables.c
+++ Tables.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 int main()
+
 {
- int n,i,k,m;
- scanf("%d%d",&n,&m);
- for(i=1;i<=m;i=i+2)
- {
-  k=n*i;
-  printf("%d x %d = %d
-",n,i,k);
- }
+
+int n,m,i;
+
+scanf("%d %d", &n,&m);
+
+for(i=1;i<=m;i++)
+
+{
+
+if(i%2!=0)
+
+printf("%d x %d = %d
+",n,i,n*i);
+}
 }
