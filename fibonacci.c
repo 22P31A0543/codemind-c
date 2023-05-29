b1dd94ecfc840b09bf540df9cff2a02121468186
Yasaswini Vulisetti <119730496+22P31A0543@users.noreply.github.com>
2023-05-29 11:54:34 +0530

    #include<stdio.h> int main() {     int i,n,t1,t2,t3;     scanf("%d",&n);     t1=0;     t2=1;     printf("%d %d ",t1,t2);     for(i=3;i<=n;i++)     {         t3=t1+t2;         printf("%d ",t3);         t1=t2;         t2=t3;     } }

--- fibonacci.c
+++ fibonacci.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 int main()
 {
- int n,a=0,b=1,c,i,m;//first number 0 and 1
- scanf("%d",&n);
- for(i=1;i<=n;i++)
- {
-    printf("%d ",a);
-    c=a+b;
-    a=b;
-    b=c;
- }
+    int i,n,t1,t2,t3;
+    scanf("%d",&n);
+    t1=0;
+    t2=1;
+    printf("%d %d ",t1,t2);
+    for(i=3;i<=n;i++)
+    {
+        t3=t1+t2;
+        printf("%d ",t3);
+        t1=t2;
+        t2=t3;
+    }
 }
+
+
+
+
+
