aaf94a65b6f4cafb1e7434fa7ca8545517b0f8c6
Yasaswini Vulisetti <119730496+22P31A0543@users.noreply.github.com>
2023-05-29 11:52:16 +0530

    #include<stdio.h> int main() {     int a,b,gcd,i;     scanf("%d %d",&a,&b);     for(i=1;i<=a||i<=b;i++)     {         if(a%i==0 && b%i==0)         {             gcd=i;         }     }     printf("%d",gcd); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    int i,gcd;
-    for(i=1;i<=a && i<=b;i++)
+    int a,b,gcd,i;
+    scanf("%d %d",&a,&b);
+    for(i=1;i<=a||i<=b;i++)
     {
-        if(a%i==0&&b%i==0)
+        if(a%i==0 && b%i==0)
         {
             gcd=i;
         }
     }
     printf("%d",gcd);
-    return 0;
 }
